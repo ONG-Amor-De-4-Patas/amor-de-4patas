d46e387b00d06386ed67517e0c25a73fdad64913
Nico Pierson <nicogpt@gmail.com>
2021-11-17 11:12:13 -0800

    upload photo of organization main image and info about the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,19 +23,37 @@ const IndexPage: NextPage = () => {
     <Layout title="ONG Amor de 4 Patas Home Page">
       <div className="page-container">
           <h1>Amor de 4 Patas</h1>
-          <img src='/erda-estremera-xfdVC5osEj4-unsplash.jpeg' alt='dog in shelter' height={500}/>
+          <img src='/amor-de-4-patas-organization.jpg' alt='organization in front of church' height={500}/>
           <h2>Tabuleiro Shelter Project</h2>
           <h3>
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-            <br />
-            The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+            Our Mission: 
           </h3>
-          <br />
-          <h2>About Us</h2>
-          <h4>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </h4>
-          <h1>Donate with Elements</h1>
+          <h4>To support stray animals to live a better life and bring the community together with animals</h4>
+  
+          <h2>Our Animals up for Adoption</h2>
+          <ImageDiv>
+              <Image src='/sasha-sashina-Xcscr_sNSEY-unsplash.jpeg' alt='dog in shelter' />
+              <Image src='/thomas-park-On6bRQRn5lY-unsplash.jpeg' alt='cat in shelter' />
+              <Image src='/chetan-hireholi-IEMJO7CvK7I-unsplash.jpeg' alt='shelter' />
+              <Image src='/nrd-MTHt9XoDzrg-unsplash.jpeg' alt='cat in cage' />
+          </ImageDiv>
+
+          <h2>Our Priorities</h2>
+          <p>
+            <ul>
+              <li>1. Resgatar, cuidar e proteger animais de rua.</li> 
+              <li>2. Prestar assistência aos animais de rua, em urgências e emergências. </li>
+              <li>3. Promover a castração, inicialmente, dos machos e, posteriormente, de fêmeas em situações de abandono (cães e gatos). </li>
+              <li>4. Manter parcerias com médicos veterinários, a fim de receber suporte clínico e cirúrgico. </li>
+              <li>5. Trabalhar a adoção responsável dos animais. </li>
+              <li>6. Vacinar, anualmente, os animais resgatados para raiva e para as viroses.</li>
+              <li>7. Fazer teste rápido de calazar, anualmente. </li>
+              <li>9. Promover a alimentação diária.</li>
+              <li>10. Colher hemogramas de controle anual. </li>
+            </ul>
+          </p>
+          
+          <h1>Help Support Our Efforts</h1>
           <p>Donate to our project 💖</p>
           <Link href='/home'>
             <a className='sidebar__btn_donate'>
@@ -47,13 +65,6 @@ const IndexPage: NextPage = () => {
           {/* <Elements stripe={getStripe()}>
             <ElementsForm />
           </Elements> */}
-          <h2>Pictures</h2>
-          <ImageDiv>
-              <Image src='/sasha-sashina-Xcscr_sNSEY-unsplash.jpeg' alt='dog in shelter' />
-              <Image src='/thomas-park-On6bRQRn5lY-unsplash.jpeg' alt='cat in shelter' />
-              <Image src='/chetan-hireholi-IEMJO7CvK7I-unsplash.jpeg' alt='shelter' />
-              <Image src='/nrd-MTHt9XoDzrg-unsplash.jpeg' alt='cat in cage' />
-          </ImageDiv>
       </div>
     </Layout>
   )
